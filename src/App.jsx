5eb5763674ceae424f4ddac78ecfbf4cac019419
Miranda Lucas <mirandalucas@Mirandas-MacBook-Pro.local>
2023-11-06 19:15:16 -0600

    workshop attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import './App.css'
-import puppyList from './data.js'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import './path-to-css.css'
 
 function App() {
 
-  return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
-}
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featuredPuppyId, setFeaturedPuppyId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featuredPuppyId)
+
+  console.log("puppyList:", puppyList)
+
+    return (
+      <>
+        <div className='all'>
+          <section className='puppyNames'>
+            {puppies.map((puppy) => {
+              return (
+              <p onClick={()=>{setFeaturedPuppyId(puppy.id)}} key={puppy.id}>
+                {puppy.name}
+              </p>)})
+              }
+            </section>
+          {featuredPuppyId && (
+            <section className='selectedPuppy'>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </section>
+          )}
+        </div>
+      </>
+    )
+  }
+
 
 export default App
